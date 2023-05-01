f7d02a8fe557af93fe0d5b5917182de2edaddc9b
Timko Timofey <timkotimofey@gmail.com>
2023-05-01 17:38:38 -0500

    TextBox

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,5 @@
-
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
 
 class BasePage:            # This is parent (basic) class. Other classes were inherit from this class
     def __init__(self, driver, url):
@@ -7,3 +8,19 @@ class BasePage:            # This is parent (basic) class. Other classes were in
 
     def open(self):
         self.driver.get(self.url)
+
+    def element_is_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+    def elements_are_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
+    def element_is_present(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+    def elements_are_present(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
+    def element_is_not_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
+    def element_is_clickable(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
+    def go_to_element(self, element):
+        self.driver.execute_script("argument[0].scrollIntroView();", element)
+
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,9 +1,13 @@
 import time
 
-from pages.base_page import BasePage
+from pages.elements_page import TextBoxPage
+
+class TestElements:
+    class TestTextBox:
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
+            text_box_page.open()
+            text_box_page.fill_all_fields()
+            time.sleep(5)
 
 
-def test(driver):
-    page = BasePage(driver, 'https://www.google.com')
-    page.open()
-    time.sleep(3)
