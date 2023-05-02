2c01ad960f0d9170fd96dc4956613850c1d56f31
Timko Timofey <timkotimofey@gmail.com>
2023-05-02 15:35:35 -0500

    TextBox

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -13,3 +13,10 @@ class TextBoxPage(BasePage):
         self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys('qwert')
         self.element_is_visible(self.locators.SUBMIT).click()
 
+    def check_filled_form(self):                                    # function for parsing text from page
+        full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(':')[1]
+        email = self.element_is_present(self.locators.CREATED_EMAIL).text.split(':')[1]
+        current_adress = self.element_is_present(self.locators.CREATED_CURRENT_ADDRESS).text.split(':')[1]
+        permanent_adress = self.element_is_present(self.locators.CREATED_PERMANENT_ADDRESS).text.split(':')[1]
+        return full_name, email, current_adress, permanent_adress
+
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -8,6 +8,11 @@ class TestElements:
             text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
             text_box_page.open()
             text_box_page.fill_all_fields()
-            time.sleep(5)
+            output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
+            # text_box_page.check_filled_form() - it contain tuple ('Name:Tim', 'Email:timkotimofey@gmail.com', 'Current Address :123D', 'Permananet Address :qwert')
+            print(output_name)
+            print(output_email)
+            print(output_cur_addr)
+            print(output_per_addr)
 
 
