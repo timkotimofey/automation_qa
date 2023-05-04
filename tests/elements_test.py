c0011498cbff617466c10fa7b16a5abb84c4bc77
Timko Timofey <timkotimofey@gmail.com>
2023-05-04 17:00:21 -0500

    Text Box Generate Data 3

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -10,10 +10,10 @@ class TestElements:
             text_box_page.open()
             full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
             output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
-            assert full_name == output_name
-            assert email == output_email
-            assert current_address == output_cur_addr
-            assert permanent_address == output_per_addr
+            assert full_name == output_name, "The full name does not match"
+            assert email == output_email, "The email does not match"
+            assert current_address == output_cur_addr, "The current address does not match"
+            assert permanent_address == output_per_addr, "The permanent address does not match"
 
 
 
