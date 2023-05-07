adf132ecdd869b469152e1420a296b6ab9c95dcf
Timko Timofey <timkotimofey@gmail.com>
2023-05-07 18:06:32 -0500

    fixed go_to_element in BasePage; Update elements test; Added test checkboxes;

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,8 @@
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 
-class BasePage:            # This is parent (basic) class. Other classes were inherit from this class
+
+class BasePage:  # This is parent (basic) class. Other classes were inherit from this class
     def __init__(self, driver, url):
         self.driver = driver
         self.url = url
@@ -11,16 +12,21 @@ class BasePage:            # This is parent (basic) class. Other classes were in
 
     def element_is_visible(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+
     def elements_are_visible(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
+
     def element_is_present(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+
     def elements_are_present(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
+
     def element_is_not_visible(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
+
     def element_is_clickable(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
-    def go_to_element(self, element):
-        self.driver.execute_script("argument[0].scrollIntroView();", element)
 
+    def go_to_element(self, element):
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
