7f33006deb69bae648b14f94e1735141dcfc8a4c
Timko Timofey <timkotimofey@gmail.com>
2023-05-12 11:16:33 -0500

    added tests: test_web_table_update_person_info, test_web_table_delete_person, test_web_table_change_count_row

--- debug.py
+++ debug.py
@@ -1,11 +1,25 @@
-data1 = ['Desktop', 'Notes', 'Commands', 'React', 'Office', 'Public', 'Private', 'Classified', 'General', 'Downloads', 'Word File.doc', 'Excel File.doc']
 
-data2 = ['desktop', 'notes', 'commands', 'react', 'office', 'public', 'private', 'classified', 'general', 'downloads', 'wordFile', 'excelFile']
+# ------------------------------------------
+# data1 = ['Desktop', 'Notes', 'Commands', 'React', 'Office', 'Public', 'Private', 'Classified', 'General', 'Downloads', 'Word File.doc', 'Excel File.doc']
+#
+# data2 = ['desktop', 'notes', 'commands', 'react', 'office', 'public', 'private', 'classified', 'general', 'downloads', 'wordFile', 'excelFile']
+#
+#
+# data1 = str(data1).replace(' ', '').replace('doc', '').replace('.', '').lower()
+# data2 = str(data2).replace(' ', '').lower()
+# print(data1)
+# print(data2)
+#
+# assert data1 == data2
+# -------------------------------------------
+
+from pages.base_page import BasePage
+
+class Debug(BasePage):
+
+    def debug_with_base_page(self):
 
 
-data1 = str(data1).replace(' ', '').replace('doc', '').replace('.', '').lower()
-data2 = str(data2).replace(' ', '').lower()
-print(data1)
-print(data2)
 
-assert data1 == data2
+
+
