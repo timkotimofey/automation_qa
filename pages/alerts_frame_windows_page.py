818a3a8b9ef6e1bd2a7cda03e28f408178ad65fd
Timko Timofey <timkotimofey@gmail.com>
2023-05-21 21:11:21 -0500

    added tests: test_see_alert, test_alert_appear_5_sec, test_confirm_alert, test_prompt_alert

--- pages/alerts_frame_windows_page.py
+++ pages/alerts_frame_windows_page.py
@@ -1,4 +1,7 @@
-from locators.alerts_frame_windows_locators import BrowserWindowsPageLocators
+import random
+import time
+
+from locators.alerts_frame_windows_locators import BrowserWindowsPageLocators, AlertPageLocators
 from pages.base_page import BasePage
 
 
@@ -16,6 +19,40 @@ class BrowserWindowsPage(BasePage):
         self.driver.switch_to.window(self.driver.window_handles[1])
         text_title = self.element_is_present(self.locators.TITLE_NEW).text
         return text_title
+
+
+class AlertsPage(BasePage):
+    locators = AlertPageLocators()
+
+    def check_see_alert(self):
+        self.element_is_visible(self.locators.SEE_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        return alert_window.text
+
+    def check_alert_appear_5_sec(self):
+        self.element_is_visible(self.locators.APPEAR_ALERT_AFTER_5_SEC_BUTTON).click()
+        time.sleep(6)
+        alert_window = self.driver.switch_to.alert
+        return alert_window.text
+
+    def check_confirm_alert(self):
+        self.element_is_visible(self.locators.CONFIRM_BOX_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        methods = [alert_window.dismiss, alert_window.accept]
+        random_method = random.choice(methods)
+        random_method()
+        text_result = self.element_is_present(self.locators.CONFIRM_RESULT).text
+        # print(text_result)
+        return text_result
+
+    def check_prompt_alert(self):
+        text = f"autotest{random.randint(0,999)}"
+        self.element_is_visible(self.locators.PROMPT_BOX_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        alert_window.send_keys(text)
+        alert_window.accept()
+        text_result = self.element_is_present(self.locators.PROMPT_RESULT).text
+        return text, text_result
 
 
 
--- tests/alerts_frame_windows_test.py
+++ tests/alerts_frame_windows_test.py
@@ -1,10 +1,9 @@
 import time
 
-from pages.alerts_frame_windows_page import BrowserWindowsPage
+from pages.alerts_frame_windows_page import BrowserWindowsPage, AlertsPage
 
 
 class TestAlertsFrameWindow:
-
     class TestBrowserWindows:
 
         def test_new_tab(self, driver):
@@ -18,3 +17,34 @@ class TestAlertsFrameWindow:
             browser_window_page.open()
             text_result = browser_window_page.check_opened_new_tab()
             assert text_result == 'This is a sample page', "the new window has not opened or an incorrect tab has opened"
+
+    class TestAlertPage:
+
+        def test_see_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_see_alert()
+            assert alert_text == 'You clicked a button', 'Alert did not show up'
+
+        def test_alert_appear_5_sec(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_alert_appear_5_sec()
+            assert alert_text == 'This alert appeared after 5 seconds', 'Alert did not show up'
+
+        def test_confirm_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_confirm_alert()
+            assert alert_text == 'You selected Ok' or alert_text == 'You selected Cancel', 'Alert did not show up'
+
+        def test_prompt_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            text, alert_text = alert_page.check_prompt_alert()
+            # print(text)
+            # print(alert_text)
+            assert alert_text == f'You entered {text}', 'Alert did not show up'
+
+
+
