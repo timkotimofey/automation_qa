29e0c3f96dec6ee960d10afcbe32225434a88a99
Timko Timofey <timkotimofey@gmail.com>
2024-02-11 18:20:14 -0600

    add updates libr

--- conftest.py
+++ conftest.py
@@ -9,7 +9,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture(scope="function")
 def driver():  # This function which creates driver
     # SET UP of fixture (before running our test)
-    driver = webdriver.Chrome(ChromeDriverManager().install())  # With help this command I run our browser
+    driver = webdriver.Chrome()  # With help this command I run our browser
     driver.maximize_window()  # Open window full screen
 
     # TEAR DOWN of fixture (after running our test)
